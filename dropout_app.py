12eb3ebc27e1cca97afa927f31e949480dcdafff
Nur Huda Riyantoni <124652440+hud4-yanto@users.noreply.github.com>
2025-05-14 16:11:12 +0700

    Update dropout_app.py

--- dropout_app.py
+++ dropout_app.py
@@ -105,7 +105,7 @@ with col2:
     data["Curricular_units_2nd_sem_without_evaluations"] = Curricular_units_2nd_sem_without_evaluations
  
 with col3:
-    'Curricular_units_2nd_sem_grade' = float(st.number_input(label='Curricular_units_2nd_sem_grade', value=11.27))
+    Curricular_units_2nd_sem_grade = float(st.number_input(label='Curricular_units_2nd_sem_grade', value=11.27))
     data["'Curricular_units_2nd_sem_grade'"] = 'Curricular_units_2nd_sem_grade'
  
 
