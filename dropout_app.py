670a52cd664cd3e22f0151d3907b6ae732c74016
Nur Huda Riyantoni <124652440+hud4-yanto@users.noreply.github.com>
2025-05-14 22:50:36 +0700

    Update dropout_app.py

--- dropout_app.py
+++ dropout_app.py
@@ -103,12 +103,12 @@ with st.expander("View the Raw Data"):
 
 if st.button('Predict'):
     if data.empty or data.isnull().any().any():
-    st.error("Data tidak lengkap atau ada kesalahan input. Silakan periksa kembali semua isian.")
+        st.error("Data tidak lengkap atau ada kesalahan input. Silakan periksa kembali semua isian.")
     else:
-    new_data = data_preprocessing(data=data)
-    with st.expander("View the Preprocessed Data"):
-        st.dataframe(data=new_data, width=800, height=10)
-    st.write("Status prediction: {}".format(prediction(new_data)))
+        new_data = data_preprocessing(data=data)
+        with st.expander("View the Preprocessed Data"):
+            st.dataframe(data=new_data, width=800, height=10)
+        st.write("Status prediction: {}".format(prediction(new_data)))
     # new_data = data_preprocessing(data=data)
     # with st.expander("View the Preprocessed Data"):
     #     st.dataframe(data=new_data, width=800, height=10)
